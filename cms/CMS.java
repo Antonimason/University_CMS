89316f7c499ce371ab90d6c87ebb6e958badfc1d
Antonio Giambra <elchuli3000@gmail.com>
2025-07-20 14:36:52 +0100

    CMS file upload

---
+++ cms/CMS.java
@@ -0,0 +1,18 @@
+
+package cms;
+
+import Menu.mainMenu;
+import java.sql.SQLException;
+
+
+public class CMS {
+
+    // Code has been tested and ready for releasing
+    public static void main(String[] args) throws SQLException {
+        //Creating a new instance of mainMenu
+        mainMenu menu = new mainMenu();
+        //Calling mainMenu main method in order to start the process
+        menu.startMenu();
+    }
+    
+}
